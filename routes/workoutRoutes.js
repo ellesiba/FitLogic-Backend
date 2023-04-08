6531fa73f2fe2a4fe8518628512b5f04e3366c5f
ellesiba <rochelle.sibaja@gamil.com>
2023-04-08 15:01:39 -0400

    uncommented routes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,10 +2,9 @@ const router = require('express').Router()
 const { workoutCtrl } = require('../controllers') //all of our methods instide of controllers/index.js
 
 //ROUTES - METHODS //
-
-// router.get('/', workoutCtrl.getWorkout)
-// router.post('/', workoutCtrl.createWorkout)
-// router.put("/:id", workoutCtrl.updateWorkout)
-// router.delete('/:id', workoutCtrl.deleteWorkout)
+router.get('/', workoutCtrl.getWorkout)
+router.post('/', workoutCtrl.createWorkout)
+router.put("/:id", workoutCtrl.updateWorkout)
+router.delete('/:id', workoutCtrl.deleteWorkout)
 
 module.exports = router 
