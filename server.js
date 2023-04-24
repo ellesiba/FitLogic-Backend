9d9ff49ce9490677ed9b4f38d8c0d192831fa8cd
ellesiba <rochelle.sibaja@gamil.com>
2023-04-24 15:11:48 -0400

    Deployment info

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.delete('/workouts/:id', async (req, res) => {
   res.json({ message: 'Workout deleted successfully' });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
